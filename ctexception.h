934e5eb8d1f2722a7ddbb9425644c0863bffeac7
Gary Meyer <gary@meyer.net>
1999-10-17 05:11:51 +0000

    Exception.
    svn path=/trunk/kdeadmin/kcron/; revision=30978

---
+++ ctexception.h
@@ -0,0 +1,32 @@
+/***************************************************************************
+ *   ctexception.h                                                         *
+ *   --------------------------------------------------------------------  *
+ *   CT Exception Header                                                   *
+ *   --------------------------------------------------------------------  *
+ *   Copyright (C) 1999, Gary Meyer <gary@meyer.net>                       *
+ *   --------------------------------------------------------------------  *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   * 
+ ***************************************************************************/
+
+#ifndef CTEXCEPTION_H
+#define CTEXCEPTION_H
+
+/**
+  * Base exception.
+  */
+class CTException {};
+
+/**
+  * Stream, such as file, i/o exception.
+  */
+class CTExceptionIO : public CTException {};
+
+/**
+  * Parse exception.
+  */
+class CTExceptionParse : public CTException {};
+
+#endif // CTEXCEPTION_H
