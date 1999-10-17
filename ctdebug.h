0e30563bcbe5be8ffbd100c4c0d9c3c2d740a02a
Gary Meyer <gary@meyer.net>
1999-10-17 05:10:43 +0000

    Debug.
    svn path=/trunk/kdeadmin/kcron/; revision=30975

---
+++ ctdebug.h
@@ -0,0 +1,45 @@
+/***************************************************************************
+ *   ctdebug.h                                                             *
+ *   --------------------------------------------------------------------  *
+ *   CT Debug Header                                                       *
+ *   --------------------------------------------------------------------  *
+ *   Copyright (C) 1999, Gary Meyer <gary@meyer.net>                       *
+ *   --------------------------------------------------------------------  *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   * 
+ ***************************************************************************/
+
+#ifndef CTDEBUG_H
+#define CTDEBUG_H
+
+#include <iostream.h>
+#include <string>
+
+/**
+  * Simple run-time entry/exit tracer.
+  */
+
+class CTDebug
+{
+private:
+  string context;
+
+public:
+  CTDebug(string _context = "Undefined Context")
+  {
+    cout << (context = _context) << ": begin" << endl;
+  };
+
+  ~CTDebug()
+  {
+    cout << context << ": end" << endl;
+  }
+};
+
+// #define CTDEBUG(X) CTDebug ctdebug(X);
+
+#define CTDEBUG(X)
+
+#endif // CTDEBUG_H
