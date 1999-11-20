40779ee663f7e948a1372d5a87725c95d74fc0de
Gary Meyer <gary@meyer.net>
1999-11-20 04:16:04 +0000

    Simplified #ifdefs and #includes to support KDE and KDE 1 vs. KDE 2.
    svn path=/trunk/kdeadmin/kcron/; revision=34433

--- cti18n.h
+++ cti18n.h
@@ -17,27 +17,22 @@
 // Do not introduce any Qt or KDE dependencies into the "CT"-prefixed classes.
 // I want to be able to reuse these classes with another GUI toolkit. -GM 11/99
 //
-// Note: I do make an exception to the above rule for this class, necessary
-// to support internationalization.  It is controlled with macros, but still
-// don't introduce any other Qt or KDE dependencies here. -GM 11/99
+// Note: I do make an exception to the above rule for this file, as it is
+// necessary to support internationalization.
+//
+// Regardless, please don't introduce any other Qt or KDE dependencies here.
+// -GM 11/99
 
 #ifdef KDE
+#include <klocale.h>
+#endif
+
 #ifdef KDE1
 #include <kapp.h>
-#include <klocale.h>
-#define cti18n(X) i18n(X)
-#endif
 #endif
 
-#ifdef KDE
-#ifndef KDE1
-#include <klocale.h>
-#define cti18n(X) (const char*)i18n(X)
-#endif
-#endif
-
-#ifndef cti18n
-#define cti18n(X) X
+#ifndef i18n
+#define i18n(X) X
 #endif
 
 #endif // CTI18N_H
