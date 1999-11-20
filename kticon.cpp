dcdd42d48e157dd3e8ca7f7222c13187f8345781
Gary Meyer <gary@meyer.net>
1999-11-20 04:32:21 +0000

    Moved icons into implementation file.
    svn path=/trunk/kdeadmin/kcron/; revision=34442

---
+++ kticon.cpp
@@ -0,0 +1,180 @@
+/***************************************************************************
+ *   kticon.h                                                              *
+ *   --------------------------------------------------------------------  *
+ *   KT icons implementation.                                              *
+ *   --------------------------------------------------------------------  *
+ *   Copyright (C) 1999, Gary Meyer <gary@meyer.net>                       *
+ *   --------------------------------------------------------------------  *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   * 
+ ***************************************************************************/
+
+#include "kticon.h"
+ 
+#ifdef KDE1
+#include <kapp.h>
+#else
+#include <kglobal.h>
+#endif
+
+#include <qimage.h>
+#include <kiconloader.h>
+
+#include "ktapp.xpm"
+#include "ktmapp.xpm"
+#include "ktuser.xpm"
+#include "ktmuser.xpm"
+#include "ktsystem.xpm"
+#include "ktmsystem.xpm"
+#include "ktfolder.xpm"
+#include "ktmfolder.xpm"
+#include "ktvariable.xpm"
+#include "ktmvariable.xpm"
+#include "kthome.xpm"
+#include "ktmhome.xpm"
+#include "ktmail.xpm"
+#include "ktmmail.xpm"
+#include "ktpath.xpm"
+#include "ktmpath.xpm"
+#include "ktshell.xpm"
+#include "ktmshell.xpm"
+#include "kttask.xpm"
+#include "ktmtask.xpm"
+
+QPixmap KTIcon::getMiniIcon(const QString& name)
+{
+#ifdef KDE1
+  return KApplication::getKApplication()->getIconLoader()->loadMiniIcon(name);
+#else
+  return KGlobal::iconLoader()->loadApplicationIcon(name, KIconLoader::Small);
+#endif
+}
+
+QPixmap KTIcon::getIcon(const QString& name)
+{
+#ifdef KDE1
+  return KApplication::getKApplication()->getIconLoader()->loadIcon(name);
+#else
+  return KGlobal::iconLoader()->loadIcon(name);
+#endif
+}
+
+QPixmap KTIcon::save()
+{
+  return getIcon("filefloppy.xpm");
+}
+
+QPixmap KTIcon::print()
+{
+  return getIcon("fileprint.xpm");
+}
+
+QPixmap KTIcon::cut()
+{
+  return getIcon("editcut.xpm");
+}
+
+QPixmap KTIcon::copy()
+{
+  return getIcon("editcopy.xpm");
+}
+
+QPixmap KTIcon::paste()
+{
+  return getIcon("editpaste.xpm");
+}
+
+QPixmap KTIcon::help()
+{
+  return getIcon("help.xpm");
+}
+
+QPixmap KTIcon::application(bool mini)
+{
+  if (mini)
+    return (const char**)ktmapp;
+  else
+    return (const char**)ktapp;
+}
+
+QPixmap KTIcon::system(bool mini)
+{
+  if (mini)
+    return (const char**)ktmsystem;
+  else
+    return (const char**)ktsystem;
+}
+
+QPixmap KTIcon::user(bool mini)
+{
+  if (mini)
+    return (const char**)ktmuser;
+  else
+    return (const char**)ktuser;
+}
+
+QPixmap KTIcon::variables(bool mini)
+{
+  if (mini)
+    return (const char**)ktmfolder;
+  else
+    return (const char**)ktfolder;
+}
+
+QPixmap KTIcon::variable(bool mini)
+{
+  if (mini)
+    return (const char**)ktmvariable;
+  else
+    return (const char**)ktvariable;
+}
+
+QPixmap KTIcon::mail(bool mini)
+{
+  if (mini)
+    return (const char**)ktmmail;
+  else
+    return (const char**)ktmail;
+}
+
+QPixmap KTIcon::shell(bool mini)
+{
+  if (mini)
+    return (const char**)ktmshell;
+  else
+    return (const char**)ktshell;
+}
+
+QPixmap KTIcon::home(bool mini)
+{
+  if (mini)
+    return (const char**)ktmhome;
+  else
+    return (const char**)kthome;
+}
+
+QPixmap KTIcon::path(bool mini)
+{
+  if (mini)
+    return (const char**)ktmpath;
+  else
+    return (const char**)ktpath;
+}
+
+QPixmap KTIcon::tasks(bool mini)
+{
+  if (mini)
+    return (const char**)ktmfolder;
+  else
+    return (const char**)ktfolder;
+}
+
+QPixmap KTIcon::task(bool mini)
+{
+  if (mini)
+    return (const char**)ktmtask;
+  else
+    return (const char**)kttask;
+}
--- kticon.h
+++ kticon.h
@@ -14,39 +14,8 @@
 #ifndef KTICON_H
 #define KTICON_H
  
-#include <iostream.h>
-
-#include <qimage.h>
-#ifdef KDE1
-#include <kapp.h>
-#else
-#include <kglobal.h>
-#endif
-#include <kiconloader.h>
-
-#include "ktapp.xpm"
-#include "ktmapp.xpm"
-
-#include "ktuser.xpm"
-#include "ktmuser.xpm"
-#include "ktsystem.xpm"
-#include "ktmsystem.xpm"
-#include "ktfolder.xpm"
-#include "ktmfolder.xpm"
-
-#include "ktvariable.xpm"
-#include "ktmvariable.xpm"
-#include "kthome.xpm"
-#include "ktmhome.xpm"
-#include "ktmail.xpm"
-#include "ktmmail.xpm"
-#include "ktpath.xpm"
-#include "ktmpath.xpm"
-#include "ktshell.xpm"
-#include "ktmshell.xpm"
-
-#include "kttask.xpm"
-#include "ktmtask.xpm"
+#include <qpixmap.h>
+#include <qstring.h>
 
 /**
   * Wraps all icons used by the application.
@@ -57,114 +26,26 @@ class KTIcon
 
 public:
 
-  static QPixmap getMiniIcon(const QString& name)
-  {
-#ifdef KDE1
-    return KApplication::getKApplication()->getIconLoader()->loadMiniIcon(name);
-#else
-    return KGlobal::iconLoader()->loadApplicationIcon(name, KIconLoader::Small);
-#endif
-  }
+  static QPixmap getMiniIcon(const QString& name);
+  static QPixmap getIcon(const QString& name);
+  static QPixmap save();
+  static QPixmap print();
+  static QPixmap cut();
+  static QPixmap copy();
+  static QPixmap paste();
+  static QPixmap help();
+  static QPixmap application(bool mini);
+  static QPixmap system(bool mini);
+  static QPixmap user(bool mini);
+  static QPixmap variables(bool mini);
+  static QPixmap variable(bool mini);
+  static QPixmap mail(bool mini);
+  static QPixmap shell(bool mini);
+  static QPixmap home(bool mini);
+  static QPixmap path(bool mini);
+  static QPixmap tasks(bool mini);
+  static QPixmap task(bool mini);
 
-  static QPixmap getIcon(const QString& name)
-  {
-#ifdef KDE1
-    return KApplication::getKApplication()->getIconLoader()->loadIcon(name);
-#else
-    return KGlobal::iconLoader()->loadIcon(name);
-#endif
-  }
-
-  static QPixmap save()
-    { return getIcon("filefloppy.xpm"); }
-  static QPixmap print()
-    { return getIcon("fileprint.xpm"); }
-  static QPixmap cut()
-    { return getIcon("editcut.xpm"); }
-  static QPixmap copy()
-    { return getIcon("editcopy.xpm"); }
-  static QPixmap paste()
-    { return getIcon("editpaste.xpm"); }
-  static QPixmap help()
-    { return getIcon("help.xpm"); }
-
-  static QPixmap application(bool mini)
-  {
-    if (mini)
-      return (const char**)ktmapp;
-    else
-      return (const char**)ktapp;
-  }
-  static QPixmap system(bool mini)
-  {
-    if (mini)
-      return (const char**)ktmsystem;
-    else
-      return (const char**)ktsystem;
-  }
-  static QPixmap user(bool mini)
-  {
-    if (mini)
-      return (const char**)ktmuser;
-    else
-      return (const char**)ktuser;
-  }
-  static QPixmap variables(bool mini)
-  {
-    if (mini)
-      return (const char**)ktmfolder;
-    else
-      return (const char**)ktfolder;
-  }
-  static QPixmap variable(bool mini)
-  {
-    if (mini)
-      return (const char**)ktmvariable;
-    else
-      return (const char**)ktvariable;
-  }
-  static QPixmap mail(bool mini)
-  {
-    if (mini)
-      return (const char**)ktmmail;
-    else
-      return (const char**)ktmail;
-  }
-  static QPixmap shell(bool mini)
-  {
-    if (mini)
-      return (const char**)ktmshell;
-    else
-      return (const char**)ktshell;
-  }
-  static QPixmap home(bool mini)
-  {
-    if (mini)
-      return (const char**)ktmhome;
-    else
-      return (const char**)kthome;
-  }
-  static QPixmap path(bool mini)
-  {
-    if (mini)
-      return (const char**)ktmpath;
-    else
-      return (const char**)ktpath;
-  }
-  static QPixmap tasks(bool mini)
-  {
-    if (mini)
-      return (const char**)ktmfolder;
-    else
-      return (const char**)ktfolder;
-  }
-  static QPixmap task(bool mini)
-  {
-    if (mini)
-      return (const char**)ktmtask;
-    else
-      return (const char**)kttask;
-  }
 };
 
 #endif // KTICON_H
