77d39347a46ad7face3b3f46c12737367b415cb4
Oswald Buddenhagen <ossi@kde.org>
2007-03-26 15:07:10 +0000

    KProcess -> K3Process KShellProcess -> K3ShellProcess KProcIO -> K3ProcIO KProcessController -> K3ProcessController
    not deprecating, as we don't have a replacement yet.
    not moving yet, as kdelibs still has heavy dependencies on it.
    
    agreed upon with dfaure.
    
    
    svn path=/trunk/KDE/kdeadmin/kcron/; revision=646732

--- ctcron.cpp
+++ ctcron.cpp
@@ -24,7 +24,7 @@
 
 #include <QFile>
 
-#include <kprocess.h>
+#include <k3process.h>
 #include <klocale.h>
 #include <ktemporaryfile.h>
 
@@ -50,15 +50,15 @@ CTCron::CTCron(const QString& crontabBinary, bool _syscron, string _login) :
   {
     if (syscron)
     {
-      readCommand  = "cat /etc/crontab > " + KProcess::quote(tmpFileName);
-      writeCommand = "cat " + KProcess::quote(tmpFileName) + " > /etc/crontab";
+      readCommand  = "cat /etc/crontab > " + K3Process::quote(tmpFileName);
+      writeCommand = "cat " + K3Process::quote(tmpFileName) + " > /etc/crontab";
       login = (const char *)i18n("(System Crontab)").toLocal8Bit();
       name = "";
     }
     else
     {
-      readCommand  = crontab + " -u " + _login.c_str() + " -l > " + KProcess::quote(tmpFileName);
-      writeCommand = crontab + " -u " + _login.c_str() + " " + KProcess::quote(tmpFileName);
+      readCommand  = crontab + " -u " + _login.c_str() + " -l > " + K3Process::quote(tmpFileName);
+      writeCommand = crontab + " -u " + _login.c_str() + " " + K3Process::quote(tmpFileName);
       if (!initFromPasswd(getpwnam(_login.c_str())))
       {
          error = i18n("No password entry found for user '%1'", _login.c_str());
@@ -68,8 +68,8 @@ CTCron::CTCron(const QString& crontabBinary, bool _syscron, string _login) :
   else
   // regular user, so provide user's own crontab
   {
-    readCommand  = crontab + " -l > " + KProcess::quote(tmpFileName);
-    writeCommand = crontab + " "      + KProcess::quote(tmpFileName);
+    readCommand  = crontab + " -l > " + K3Process::quote(tmpFileName);
+    writeCommand = crontab + " "      + K3Process::quote(tmpFileName);
     if (!initFromPasswd(getpwuid(uid)))
     {
       error = i18n("No password entry found for uid '%1'", uid);
@@ -108,8 +108,8 @@ CTCron::CTCron(const QString& crontabBinary, const struct passwd *pwd) :
   tmp.open();
   tmpFileName = tmp.fileName();  
 
-  QString readCommand  = crontab + " -u " + QString(pwd->pw_name) + " -l > " + KProcess::quote(tmpFileName);
-  writeCommand = crontab +  " -u " + QString(pwd->pw_name) + " " + KProcess::quote(tmpFileName);
+  QString readCommand  = crontab + " -u " + QString(pwd->pw_name) + " -l > " + K3Process::quote(tmpFileName);
+  writeCommand = crontab +  " -u " + QString(pwd->pw_name) + " " + K3Process::quote(tmpFileName);
 
   initFromPasswd(pwd);
 
