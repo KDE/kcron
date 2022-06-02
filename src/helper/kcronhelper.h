2ba816a0a3730349566d5b0a4058416d2cdf4c13
Laurent Montel <montel@kde.org>
2022-06-02 13:32:46 +0200

    Check is not necessary as we depend against new kf5

--- src/helper/kcronhelper.h
+++ src/helper/kcronhelper.h
@@ -23,14 +23,10 @@
  * ======================================================================== */
 
 #pragma once
-#include <QObject>
-#include <kauth_version.h>
-#if KAUTH_VERSION >= QT_VERSION_CHECK(5, 92, 0)
 #include <KAuth/ActionReply>
 #include <KAuth/HelperSupport>
-#else
-#include <KAuth>
-#endif
+#include <QObject>
+#include <kauth_version.h>
 
 using namespace KAuth;
 
